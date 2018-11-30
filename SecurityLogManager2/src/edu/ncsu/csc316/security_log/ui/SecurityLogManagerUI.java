c6626e65b9d2c970b3aec3b02659f93d4453fc78
Ryan Alexander <Ryan Alexander@DESKTOP-BSV2IHD>
2018-11-30 16:17:18 -0500

    Updated SecurityLogManagerUI

--- SecurityLogManager2/src/edu/ncsu/csc316/security_log/ui/SecurityLogManagerUI.java
+++ SecurityLogManager2/src/edu/ncsu/csc316/security_log/ui/SecurityLogManagerUI.java
@@ -2,7 +2,6 @@ package edu.ncsu.csc316.security_log.ui;
 
 import java.util.Scanner;
 
-import edu.ncsu.csc316.rentals.manager.VehicleRentalManager;
 import edu.ncsu.csc316.security_log.manager.SecurityLogManager;
 
 /**
@@ -17,35 +16,71 @@ public class SecurityLogManagerUI {
 	 * @param args commands
 	 */
 	public static void main(String args[]) {
-		System.out.print("Please input a valid file: ");
+		//Examples of valid files can be found in the input folder
+		Scanner scan = new Scanner(System.in);
+		SecurityLogManager instance = null;
 		while ( instance == null ) {
 			System.out.print("Input Rental File name: ");
-			String rentals = scan.nextLine();
-			try { 
-				instance = new VehicleRentalManager(rentals);
-			} catch ( IllegalArgumentException e ) {
-				System.out.println("Invalid file name, try something in the form: \"parentfolder/filename.csv\"");
-			}
-		}
-		try {
-			Scanner scan = new Scanner(System.in);
 			String input = scan.nextLine();
-			SecurityLogManager instance = new SecurityLogManager(input);
-			System.out.println("Enter command:");
-			String command = scan.nextLine();
-			if (command.equals("g")) {
-				System.out.println("Input start time and end time");
-				String start = scan.nextLine();
-				String end = scan.nextLine();
-				System.out.println(instance.generateOperationalProfile(start, end));
-			} else if (command.equals("u")) {
-				System.out.println("Input username");
-				String username = scan.nextLine();
-				System.out.println(instance.getUserReport(username));
+			try { 
+				instance = new SecurityLogManager(input);
+			} catch ( IllegalArgumentException e ) {
+				System.out.println("Invalid file name, try something in the form: \"parentfolder/filename.txt\"");
 			}
-			scan.close();
-		} catch (IllegalArgumentException e) {
-			System.out.println("Invalid File");
 		}
+		System.out.println( "Welcome to the Security Log Manager. To review the commands, please use the command \"help\"");
+		System.out.println();
+		String command = "";
+		while( !command.equals("quit") ) {
+			System.out.print("Enter command: ");
+			command = scan.nextLine().toLowerCase();
+			if(command.equals("profile")) {
+				String start = "";
+				String end = "";
+				while ( start.equals("") && end.equals("") ) {
+					try {
+						System.out.print("Enter start time: ");
+						start = scan.nextLine();	
+						System.out.print("Enter end time: ");
+						end = scan.nextLine();			
+						System.out.println(instance.generateOperationalProfile(start, end));
+					} catch ( IllegalArgumentException e) {
+						System.out.println();
+						System.out.println("Invalid time, please enter in the form: MM/DD/YYYY HH:MM:SSXM");
+						start = "";
+						end = "";
+					} catch (StringIndexOutOfBoundsException e) {
+						System.out.println();
+						System.out.println("Invalid time, please enter in the form: MM/DD/YYYY HH:MM:SSXM");
+						start = "";
+						end = "";
+					}
+				}
+			} else if (command.equals("report")) {
+				String username = "";
+				while ( username.equals("") ) {
+					System.out.print("Enter username: ");
+					try {
+						username = scan.nextLine();
+						System.out.println(instance.getUserReport(username));
+					} catch ( IllegalArgumentException e) {
+						System.out.println("Invalid username, please try again");
+						username = "";
+					}
+				}
+			} else if (command.equals("quit")) {
+				System.out.println("Thank you for using the Security Log Manager.");
+			} else if (command.equals("help")) {
+				System.out.println("Command Options:");
+				System.out.println("Profile - generate an Operational Profile between two times");
+				System.out.println("Report - generate a User Report for a particular username");
+				System.out.println("Quit - exit the Security Log Manager");
+				System.out.println();
+			} else {
+				System.out.println("Invalid Command");
+			}
+		}
+		scan.close();
 	}
 }
+
